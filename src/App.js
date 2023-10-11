093ae784c8942407c234cebf402a81a362914860
francolgonzalez <francogonzalez038@gmail.com>
2023-10-11 19:07:47 -0300

    aproximamos el proyecto para la primer entrega

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import Home from '../src/Components/Home';
 import './App.css';
-import PerfilCard from './Components/PerfilCard';
-import ProductCard from './Components/ProductCard';
-import Contador from './Components/Contador';
-import ContRef from './Components/ContRef';
-import User from './Components/User';
+import NavBar from './Components/NavBar/NavBar';
+import CartWidget from './Components/CartWidget/CartWidget';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+
 
 function App() {
   return (
     <div>
-      <Home/>
-      <User/>
-      <Contador/>
-      <ContRef/>
-      <ProductCard img='https://w7.pngwing.com/pngs/394/269/png-transparent-ikea-table-furniture-billionaire-house-painter-and-decorator-black-simple-small-table-angle-white-black-hair-thumbnail.png' price={22000} title='Mesa Comedor'>
-
-        <p>Elegante mesa con descripción hecha con children</p>
-        <button>Agregar al carrito</button>
-
-      </ProductCard>
-      <ProductCard img='https://images.fravega.com/f1000/8cd950d8d167aa41a5f2ec957c8082ee.jpg' price={10000} title='Silla Comedor'>
-
-        <p>Elegante silla con descripción hecha con children</p>
-        <button>Agregar al carrito</button>
-        
-      </ProductCard>
-      
-      
-      {/* <PerfilCard imagen=''/>
-      <PerfilCard imagen=''/> */}
-      <PerfilCard imagen='' nombre='Pedro' edad={35} cargo='Backend'/>
+      <NavBar/>
+      <CartWidget/>
+      <ItemListContainer greeting= '¡Bienvenidos!'/>
     </div>
 );
 }
---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BsCart } from "react-icons/bs";;
+
+const CartWidget = () => {
+  return (
+    <div>
+        <button>
+          <BsCart/>
+        </button>
+    </div>
+  )
+}
+
+export default CartWidget
