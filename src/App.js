98edda8a1fd296f33fdd9964cd7ea063f68b3298
francolgonzalez <francogonzalez038@gmail.com>
2023-10-11 17:24:45 -0300

    empezamos2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import Home from '../src/Components/Home';
 import './App.css';
+import PerfilCard from './Components/PerfilCard';
+import ProductCard from './Components/ProductCard';
+import Contador from './Components/Contador';
+import ContRef from './Components/ContRef';
+import User from './Components/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home/>
+      <User/>
+      <Contador/>
+      <ContRef/>
+      <ProductCard img='https://w7.pngwing.com/pngs/394/269/png-transparent-ikea-table-furniture-billionaire-house-painter-and-decorator-black-simple-small-table-angle-white-black-hair-thumbnail.png' price={22000} title='Mesa Comedor'>
+
+        <p>Elegante mesa con descripción hecha con children</p>
+        <button>Agregar al carrito</button>
+
+      </ProductCard>
+      <ProductCard img='https://images.fravega.com/f1000/8cd950d8d167aa41a5f2ec957c8082ee.jpg' price={10000} title='Silla Comedor'>
+
+        <p>Elegante silla con descripción hecha con children</p>
+        <button>Agregar al carrito</button>
+        
+      </ProductCard>
+      
+      
+      {/* <PerfilCard imagen=''/>
+      <PerfilCard imagen=''/> */}
+      <PerfilCard imagen='' nombre='Pedro' edad={35} cargo='Backend'/>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='containerParrafo'>
+        <p className='parrafo'>Hello World 🦾</p>
+    </div>
+  )
+}
+
+export default Home
